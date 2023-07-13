b6b58d989e6ad90c481f4607431fb47640e04ccc
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2023-07-13 17:42:11 +0700

    payment add bottleneck and error middleware

--- payment-service/main.go
+++ payment-service/main.go
@@ -8,14 +8,17 @@ import (
 	"os"
 	"skripsi-rizkal/payment-service/entity"
 	"skripsi-rizkal/payment-service/handler"
+	"skripsi-rizkal/payment-service/helper"
 	"skripsi-rizkal/payment-service/pkg/tracer"
 	"skripsi-rizkal/payment-service/repository"
 	"skripsi-rizkal/payment-service/service"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/uptrace/opentelemetry-go-extra/otelgorm"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -27,20 +30,39 @@ func dbMigration(db *gorm.DB) {
 	}
 }
 
+// create middleware
+func bottleNeckMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		time.Sleep(5 * time.Second)
+		return next(c)
+	}
+}
+
+func errorMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		resp := helper.BuildResponse(http.StatusInternalServerError, "Internal Server Error - This is generated error for testing",nil)
+		return c.JSON(http.StatusInternalServerError, resp)
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	tp, err := tracer.InitTracer()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		if err := tp.Shutdown(context.Background()); err != nil {
-			log.Printf("Error shutting down tracer provider: %v", err)
+	isTracingEnabled := os.Getenv("IS_TRACING_ENABLED")
+	var tp *sdktrace.TracerProvider
+	if isTracingEnabled == "true" {
+		tp, err = tracer.InitTracer()
+		if err != nil {
+			log.Fatal(err)
 		}
-	}()
+		defer func() {
+			if err := tp.Shutdown(context.Background()); err != nil {
+				log.Printf("Error shutting down tracer provider: %v", err)
+			}
+		}()
+	}
 	fmt.Println("Hello World")
 	dsn := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -55,13 +77,23 @@ func main() {
 	paymentHandler := handler.NewPaymentHandler(paymentService)
 
 	e := echo.New()
-	e.Use(otelecho.Middleware(tracer.ServiceName))
-
-	
-	if err := db.Use(otelgorm.NewPlugin(
-		otelgorm.WithTracerProvider(tp),
-	)); err != nil {
-		panic(err)
+	if isTracingEnabled == "true" {
+		e.Use(otelecho.Middleware(tracer.ServiceName))
+	}
+	isErrorEnabled := os.Getenv("IS_ERROR_ENABLED")
+	if isErrorEnabled == "true" {
+		e.Use(errorMiddleware)
+	}
+	isBottleNeckEnabled := os.Getenv("IS_BOTTLENECK_ENABLED")
+	if isBottleNeckEnabled == "true" {
+		e.Use(bottleNeckMiddleware)
+	}
+	if isTracingEnabled == "true" {
+		if err := db.Use(otelgorm.NewPlugin(
+			otelgorm.WithTracerProvider(tp),
+		)); err != nil {
+			panic(err)
+		}
 	}
 	
 	paymentGroup := e.Group("api/payments")
